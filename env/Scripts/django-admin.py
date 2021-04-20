44252155fed3aadee9cdcb7c774b96476bbc712c
luvmakin01 <57311008+luvmakin01@users.noreply.github.com>
2021-04-20 15:06:52 +0530

    landing page bootstrap

---
+++ env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\luv\desktop\django_projects\env\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ myapp/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+def index(request):
+    return render(request, 'index.html', {})
+
+def register(request):
+    return render(request, 'login.html', {})
